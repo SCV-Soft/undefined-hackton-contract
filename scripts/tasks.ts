1c2939fe4b062f8a2aa8019d72d9706129d54a22
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2023-05-25 21:39:49 +0000

    chore: Add L2 swap task

--- scripts/tasks.ts
+++ scripts/tasks.ts
@@ -1,19 +1,20 @@
 import { task } from "hardhat/config";
 
 import L1SwapAbi from "../artifacts/contracts/L1Swap.sol/L1Swap.json"
+import L2SwapAbi from "../artifacts/contracts/L2Swap.sol/L2Swap.json"
 import vethAbi from "../artifacts/contracts/VETH.sol/VETH.json"
 import wethAbi from "../artifacts/contracts/WETH.sol/WETH9.json"
 
 const l1SwapAddress = "0x7C216fB3C5C22989d0D2556702ea7AeCF474245f"
-const l2SwapAddress = ""
+const l2SwapAddress = "0xDA49F943Be939Ef9eE1BdaB3C9D1644Baae763bb"
 
 const l1WethAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"
-const l2WethAddress = ""
+const l2WethAddress = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa"
 
 const l1VethAddress = "0xfC6ae96facE347BB6419859C1592825B96224ab0"
 const l2VethAddress = "0xe5b1C4Be4289CA511440C1287E0C9E031a3bfe3D"
 
-const scethAddress = ""
+const scethAddress = "0x153fab4B5E067724B4387713ABfBB6Eb581119d6"
 
 const lidoAddress = "0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F"
 
@@ -51,6 +52,24 @@ task("swap-l1-weth", "Swap L1 WETH to L1 vETH")
     });
 
 
+// L2 스왑(WETH)
+// @todo 프론트에서 호출해야 함 (폴리곤(뭄바이)에서 실행)
+task("swap-l2-weth", "Swap L2 WETH to L2 vETH + scETH")
+    .addParam("amount", "amount of tokens (wei)")
+    .setAction(async (args, { ethers, network }) => {
+        const accounts = await ethers.getSigners();
+        const admin = accounts[0];
+        const user = accounts[1];
+        // 사용성을 올리려면 view 함수로 allowance 체크 
+        const weth = new ethers.Contract(l2WethAddress, wethAbi.abi, user);
+        await (await weth.approve(l2SwapAddress, args.amount)).wait();
+
+        const l2swap = new ethers.Contract(l2SwapAddress, L2SwapAbi.abi, user);
+
+        await (await l2swap.swap(args.amount)).wait()
+    });
+
+
 
 
 
@@ -65,5 +84,18 @@ task("l1-add-minter-veth", "Swap L1 ETH to L1 vETH")
         const veth = new ethers.Contract(l1VethAddress, vethAbi.abi, admin);
 
         await (await veth.addMinter(args.minter)).wait()
+    });
+
+// VETH mint
+task("deposit-l2swap", "Deposit(mint) vETH to l2Swap")
+    .addParam("amount", "amount of tokens (wei)")
+    .setAction(async (args, { ethers, network }) => {
+        const accounts = await ethers.getSigners();
+        const admin = accounts[0];
+        const user = accounts[1];
+
+        const veth = new ethers.Contract(l2VethAddress, vethAbi.abi, admin);
+
+        await (await veth.mint(l2SwapAddress, args.amount)).wait()
     });
 
